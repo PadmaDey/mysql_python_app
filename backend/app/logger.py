2b0e7234da90683a86f13eb817113c00c1b0c792
Subhadeep Banerjee <subhadeep@klizos.com>
2025-05-22 15:53:06 +0530

    Format config and logger

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -1,15 +1,13 @@
-import os
 import logging
+import os
 
 log_file_path = os.path.join("logs", "app.log")
 os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
-log_format =(
-    "%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(funcName)s | %(message)s"
-)
+log_format = "%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(funcName)s | %(message)s"
 
 ENABLE_CONSOLE_LOG = False
-handlers = [logging.FileHandler(log_file_path, mode='a')]
+handlers = [logging.FileHandler(log_file_path, mode="a")]
 if ENABLE_CONSOLE_LOG:
     handlers.append(logging.StreamHandler())
 
