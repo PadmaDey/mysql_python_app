4650de408368b93466d36df6afce61dc99aaa3b1
Subhadeep Banerjee <subhadeep@klizos.com>
2025-05-22 15:45:17 +0530

    Add configs

---
+++ backend/app/config.py
@@ -0,0 +1,22 @@
+from starlette.config import Config
+from pydantic import BaseSettings
+
+config = Config(".env")
+
+class Settings(BaseSettings):
+    """
+    Add all the environment variables to the class
+    """
+    # Server Configs
+    PORT: int = config("PORT", default=8000)
+    DEBUG: bool = config("DEBUG", default=False)
+    
+    # Database configs
+    MYSQL_DATABASE: str = config("MYSQL_DATABASE")
+    MYSQL_USER: str = config("MYSQL_USER")
+    MYSQL_PASSWORD: str = config("MYSQL_PASSWORD")
+    
+    MYSQL_HOST: str = config("MYSQL_HOST", default="mysql")
+    MYSQL_PORT: int = config("MYSQL_PORT", default=3306)
+
+settings = Settings()
