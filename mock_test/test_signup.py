621e84cd8a862e93754e3b65ebf8f57ec3522c71
deypadma <padma@klizos.com>
2025-06-24 18:02:33 +0530

    add dynamic cleanup function and modify some test case function name

--- mock_test/test_signup.py
+++ mock_test/test_signup.py
@@ -1,14 +1,18 @@
 import pytest
+from conftest import register_test_email
+
 
 # Valid signup
 @pytest.mark.asyncio
-async def test_signup_success(test_client, cleanup_user):
+async def test_signup_success(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
         "phone_no": 1234567890,
         "password": "Test@123"
     }
+
+    register_test_email(payload["email"])
 
     response = await test_client.post("/api/users/signup", json=payload)
 
@@ -17,28 +21,32 @@ async def test_signup_success(test_client, cleanup_user):
         "msg": "User created successfully",
         "status": True
     }
+
 
 # Duplicate signup
 @pytest.mark.asyncio
-async def test_signup_duplicate_email(test_client, cleanup_user):
+async def test_signup_duplicate_email(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
         "phone_no": 1234567890,
         "password": "Test@123"
-    } 
+    }
+
+    register_test_email(payload["email"])
 
     await test_client.post("/api/users/signup", json=payload)
-
     response = await test_client.post("/api/users/signup", json=payload)
+
     assert response.status_code == 409
-    assert response.json() =={
+    assert response.json() == {
         "detail": "A user with this mail already exists."
     }
 
-# Missing fields
+
+# Missing 'name'
 @pytest.mark.asyncio
-async def test_signup_missing_fields(test_client, cleanup_user):
+async def test_signup_missing_name(test_client):
     payload = {
         "email": "testuser@example.com",
         "phone_no": 1234567890,
@@ -49,8 +57,10 @@ async def test_signup_missing_fields(test_client, cleanup_user):
     assert response.status_code == 422
     assert "detail" in response.json()
 
+
+# Missing 'email'
 @pytest.mark.asyncio
-async def test_signup_missing_fields(test_client, cleanup_user):
+async def test_signup_missing_email(test_client):
     payload = {
         "name": "Test User1",
         "phone_no": 1234567890,
@@ -61,13 +71,19 @@ async def test_signup_missing_fields(test_client, cleanup_user):
     assert response.status_code == 422
     assert "detail" in response.json()
 
+
+# Missing 'phone_no'
 @pytest.mark.asyncio
-async def test_signup_missing_fields(test_client, cleanup_user):
+async def test_signup_missing_phone_no(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
         "password": "Test@123"
     }
+
+    register_test_email(payload["email"])
+
+    response = await test_client.post("/api/users/signup", json=payload)
 
     assert response.status_code == 201
     assert response.json() == {
@@ -75,8 +91,10 @@ async def test_signup_missing_fields(test_client, cleanup_user):
         "status": True
     }
 
+
+# Missing 'password'
 @pytest.mark.asyncio
-async def test_signup_missing_fields(test_client):
+async def test_signup_missing_password(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
@@ -87,12 +105,13 @@ async def test_signup_missing_fields(test_client):
     assert response.status_code == 422
     assert "detail" in response.json()
 
-# Invalid email
+
+# Invalid email format
 @pytest.mark.asyncio
 async def test_signup_invalid_email_format(test_client):
     payload = {
         "name": "Test User1",
-        "email": "testuserexample.com",
+        "email": "testuserexample.com",  # Missing '@'
         "phone_no": 1234567890,
         "password": "Test@123"
     }
@@ -100,6 +119,7 @@ async def test_signup_invalid_email_format(test_client):
     response = await test_client.post("/api/users/signup", json=payload)
     assert response.status_code == 422
     assert "detail" in response.json()
+
 
 # Weak password
 @pytest.mark.asyncio
@@ -108,39 +128,43 @@ async def test_signup_weak_password(test_client):
         "name": "Test User1",
         "email": "testuser@example.com",
         "phone_no": 1234567890,
-        "password": "Test123"
+        "password": "Test123"  # No special char
     }
 
     response = await test_client.post("/api/users/signup", json=payload)
     assert response.status_code == 422
     assert "detail" in response.json()
     assert any(
-        "Password must be at least 8 characters long, include an uppercase letter, "
-        "a lowercase letter, a number, and a special character." in err["msg"] 
+        "Password must be at least 8 characters long, include an uppercase letter," in err["msg"]
         for err in response.json()["detail"]
     )
 
-# Invalid contact number
+
+# Invalid phone number (too short)
 @pytest.mark.asyncio
-async def test_signup_invalid_phone_number(test_client):
+async def test_signup_invalid_short_phone_number(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
-        "phone_no": 123456789,
+        "phone_no": 123456789,  # 9 digits
         "password": "Test@123"
     }
+
     response = await test_client.post("/api/users/signup", json=payload)
     assert response.status_code == 422
     assert "detail" in response.json()
 
+
+# Invalid phone number (string type)
 @pytest.mark.asyncio
-async def test_signup_invalid_phone_number(test_client):
+async def test_signup_invalid_phone_string(test_client):
     payload = {
         "name": "Test User1",
         "email": "testuser@example.com",
-        "phone_no": "1234567890",
+        "phone_no": "1234567890",  # should be int
         "password": "Test@123"
     }
+
     response = await test_client.post("/api/users/signup", json=payload)
     assert response.status_code == 422
     assert "detail" in response.json()
