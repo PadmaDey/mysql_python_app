ad960d71720bc095767f4512b097b4f8dc1253e3
deypadma <padma@klizos.com>
2025-06-24 15:17:36 +0530

    add a condition, to test in local, and actual run on docker mysql db

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,4 @@
+import os
 from pydantic_settings import BaseSettings
 from starlette.config import Config
 
@@ -18,15 +19,26 @@ class Settings(BaseSettings):
     MYSQL_USER: str = config("MYSQL_USER", default='emp_1')
     MYSQL_PASSWORD: str = config("MYSQL_PASSWORD", default='qwerty')
 
+    
+    MYSQL_PORT: int = config("MYSQL_PORT", cast=int, default=3306)
     # MYSQL_HOST: str = config("MYSQL_HOST", default="localhost")
     MYSQL_HOST: str = config("MYSQL_HOST", default="mysql")
-    MYSQL_PORT: int = config("MYSQL_PORT", cast=int, default=3306)
+
+    # @property
+    # def ASYNC_DB_URL(self) -> str:
+    #     return (
+    #         f"mysql+asyncmy://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}"
+    #         f"@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}"
+    #     )
 
     @property
     def ASYNC_DB_URL(self) -> str:
+        actual_host = (
+            "localhost" if os.getenv("ENV") == "local" else self.MYSQL_HOST
+        )
         return (
             f"mysql+asyncmy://{self.MYSQL_USER}:{self.MYSQL_PASSWORD}"
-            f"@{self.MYSQL_HOST}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}"
+            f"@{actual_host}:{self.MYSQL_PORT}/{self.MYSQL_DATABASE}"
         )
 
 
