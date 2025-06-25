7b7b3b93f6bc7aec6ed068bcaaaa6264703e6174
deypadma <padma@klizos.com>
2025-06-25 14:26:02 +0530

    Update the import statement to reflect the renamed module.

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import signup, login, view_all_users, view_current_user, update_data, delete_data, logout
+from app.api.routes import signup, login, view_all_users, view_current_user, update_user_data, delete_data, logout
 
 api_router = APIRouter()
 
@@ -7,6 +7,6 @@ api_router.include_router(signup.router)
 api_router.include_router(login.router)
 api_router.include_router(view_all_users.router)
 api_router.include_router(view_current_user.router)
-api_router.include_router(update_data.router)
+api_router.include_router(update_user_data.router)
 api_router.include_router(delete_data.router)
 api_router.include_router(logout.router)
