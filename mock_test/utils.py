531bdf1082ba1ed8f6e93cd9571560093a717110
deypadma <padma@klizos.com>
2025-06-26 12:53:33 +0530

    create this file for signup and login and it can be use for other test files

---
+++ mock_test/utils.py
@@ -0,0 +1,21 @@
+from mock_test.conftest import register_test_email
+
+async def create_user_and_get_token(test_client, email="testuser@example.com", password="Test@123"):
+    signup_payload = {
+        "name": "Test User1",
+        "email": email,
+        "phone_no": 1234567890,
+        "password": password
+    }
+
+    await register_test_email(email)
+    await test_client.post("/api/users/signup", json=signup_payload)
+
+    login_payload = {
+        "email": email,
+        "password": password
+    }
+
+    response = await test_client.post("/api/users/login", json=login_payload)
+    data = response.json()
+    return data["token"]
