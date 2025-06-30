2aa69a40b1f38691298e424da47bd46858262145
deypadma <padma@klizos.com>
2025-06-30 13:27:42 +0530

    update datatype of phone_no from int to bigint because getting error - out of range

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, TIMESTAMP
+from sqlalchemy import BigInteger, Integer, String, TIMESTAMP
 from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column
 from app.db.database import Base
@@ -11,7 +11,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
-    phone_no: Mapped[Optional[int]] = mapped_column(Integer,)
+    phone_no: Mapped[Optional[int]] = mapped_column(BigInteger,)
     password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
 
     created_at: Mapped[TIMESTAMP] = mapped_column(TIMESTAMP, server_default=func.now())
