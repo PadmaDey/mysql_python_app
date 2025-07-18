2dfab99783ebb957a8401c832b9e57e6f6cbf75e
deypadma <padma@klizos.com>
2025-07-18 20:49:43 +0530

    add pytest, comment out unittest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,28 @@
-import unittest
-from httpx import AsyncClient
-from httpx import ASGITransport
+import pytest
 
-from backend.app.main import app
+@pytest.mark.asyncio
+async def test_read_root(test_client):
+    response = await test_client.get("/api/healthcheck")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "The API is LIVE!!"}
 
 
-class TestMainApp(unittest.IsolatedAsyncioTestCase):
 
-    async def test_healthcheck_endpoint(self):
-        transport = ASGITransport(app=app)
-        async with AsyncClient(transport=transport, base_url="http://test") as ac:
-            response = await ac.get("/api/healthcheck")
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"msg": "The API is LIVE!!"})
+
+# import unittest
+# from httpx import AsyncClient
+# from httpx import ASGITransport
+
+# from backend.app.main import app
+
+
+# class TestMainApp(unittest.IsolatedAsyncioTestCase):
+
+#     async def test_healthcheck_endpoint(self):
+#         transport = ASGITransport(app=app)
+#         async with AsyncClient(transport=transport, base_url="http://test") as ac:
+#             response = await ac.get("/api/healthcheck")
+
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.json(), {"msg": "The API is LIVE!!"})
