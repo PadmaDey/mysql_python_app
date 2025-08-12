af69fa7fed2d1e10f11d7b997930b9cf2b95c452
Padma Dey <padma@klizos.com>
2025-08-12 11:14:55 +0000

    add load test

--- load_tests/end_point_test_entry/locustfile_signup.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, between, task
-from load_tests.endpoints.signup_test import SignupUserMixin
-
-class SignupUserTest(SignupUserMixin, HttpUser):
-    wait_time = between(1, 3)
-
-    @task
-    def signup(self):
-        self.signup_user()
